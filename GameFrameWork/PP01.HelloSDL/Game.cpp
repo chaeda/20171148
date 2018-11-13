811951cd7321e2407ca80f5a0bb2c0b8843173eb
chaeda <20171148@game.hoseo.edu>
2018-11-13 13:10:56 +0900

    PP14.MInputHandler

--- GameFrameWork/PP01.HelloSDL/Game.cpp
+++ GameFrameWork/PP01.HelloSDL/Game.cpp
@@ -1,16 +1,19 @@
 #include "Game.h"
+#include "InputHandler.h"
 #include "TextureManager.h"
-#include <iostream>
-using namespace std;
+#include "Player.h"
+#include "Enemy.h"
 
-Game* Game::s_pInstance = NULL;
+Game* Game::s_pInstance = 0;
 
-bool Game::init(const char* title, int xpos, int ypos,
-	int width, int height, bool fullscreen)
+bool Game::init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen)
 {
+
 	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
 	{
-		m_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, SDL_WINDOW_SHOWN);
+
+		m_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, fullscreen);
+
 		if (m_pWindow != 0)
 		{
 			m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
@@ -18,26 +21,30 @@ bool Game::init(const char* title, int xpos, int ypos,
 
 		m_bRunning = true;
 
-		if (!TextureManager::Instance()->load("assets/animate-alpha.png", "animate", m_pRenderer))
+		if (!TheTextureManager::Instance()->load("assets/animate-alpha.png",
+			"animate", m_pRenderer))
 		{
 			return false;
 		}
 
 		m_gameObjects.push_back(new Player(new LoaderParams(100, 100, 128, 82, "animate")));
 		m_gameObjects.push_back(new Enemy(new LoaderParams(300, 300, 128, 82, "animate")));
-		SDL_SetRenderDrawColor(m_pRenderer, 255, 0, 0, 255);
+
+
 	}
-	else
-	{
-		return false; // sdl could not initialize
+
+	else {
+		return false;
 	}
+
 	return true;
 }
 
 void Game::render()
 {
+
 	SDL_RenderClear(m_pRenderer); // clear to the draw colour
-	for (vector<GameObject*>::size_type i = 0;
+	for (std::vector<GameObject*>::size_type i = 0;
 		i != m_gameObjects.size(); i++)
 	{
 		m_gameObjects[i]->draw();
@@ -48,43 +55,29 @@ void Game::render()
 
 void Game::clean()
 {
-	cout << "cleaning game\n";
+	std::cout << "claning game \n";
 	SDL_DestroyWindow(m_pWindow);
 	SDL_DestroyRenderer(m_pRenderer);
+	TheInputHandler::Instance()->clean();
 	SDL_Quit();
 }
 
-void Game::handleEvents()
+void Game::handleEvent()
 {
-	SDL_Event event;
-	if (SDL_PollEvent(&event))
-	{
-		switch (event.type)
-		{
-		case SDL_QUIT:
-			m_bRunning = false;
-			break;
-		default:
-			break;
-		}
-	}
+	TheInputHandler::Instance()->update();
 }
 
 void Game::update()
 {
-	for (vector<GameObject*>::size_type i = 0;
+
+	for (std::vector<GameObject*>::size_type i = 0;
 		i != m_gameObjects.size(); i++)
 	{
 		m_gameObjects[i]->update();
 	}
 }
 
-Game::Game()
+void Game::quit()
 {
-
-}
-
-Game::~Game()
-{
-
+	m_bRunning = false;
 }
--- GameFrameWork/PP01.HelloSDL/Player.h
+++ GameFrameWork/PP01.HelloSDL/Player.h
@@ -1,9 +1,11 @@
 #pragma once
-#include "GameObject.h"
 #include "SDLGameObject.h"
+#include "InputHandler.h"
 
 class Player : public SDLGameObject
 {
+private:
+	void handleInput();
 public:
 	Player(const LoaderParams* pParams);
 	virtual void draw();
